5ac8fdd2d4d136ec3f5babc732b19e59674fcc87
Jonhatan Fajardo <jfajardo89@gmail.com>
2018-02-07 12:26:50 -0500

    static

--- eventus/settings.py
+++ eventus/settings.py
@@ -103,6 +103,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
